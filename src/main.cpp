2e62527d285b15448c6ab9cce65f73a5e3663cd5
jieggii <jieggii@protonmail.com>
2024-09-16 10:56:10 +0300

    main: use ViewRenderer

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,14 @@
 #include "ViewIndex.h"
 #include "ViewController.h"
 #include "debug_print.h"
+#include "ViewRenderer.h"
 #include "OperationalConfig.h"
 #include "WiFiConfig.h"
 #include "views/IdleView.h"
 #include "views/StandView.h"
 #include "config.h"
+
+#define VIEW_RENDER_INTERVAL 1000
 
 namespace Hardware {
     LCD1602 SCREEN(0x27);
@@ -19,6 +22,7 @@ namespace Hardware {
 namespace UI {
     ViewNavigator VIEW_NAVIGATOR(IDLE_VIEW_INDEX);
     ViewController VIEW_CONTROLLER;
+    ViewRenderer VIEW_RENDERER(VIEW_RENDER_INTERVAL);
 
     namespace Views {
         IdleView IDLE(&Hardware::SCREEN, &VIEW_NAVIGATOR, &Hardware::ENV_SENSOR);
@@ -46,25 +50,21 @@ void setup() {
     UI::VIEW_CONTROLLER.registerView(IDLE_VIEW_INDEX, &UI::Views::IDLE);
     UI::VIEW_CONTROLLER.registerView(STAND_VIEW_INDEX, &UI::Views::STAND);
 
-    UI::VIEW_NAVIGATOR.navigate(IDLE_VIEW_INDEX);
-
     debug_println("info: finish setup");
 }
 
 void loop() {
     // debug print free RAM:
-    debug_print("inf: free heap: ");
-    debug_print(ESP.getFreeHeap());
-    debug_println(" bytes");
+//    debug_print("inf: free heap: ");
+//    debug_print(ESP.getFreeHeap());
+//    debug_println(" bytes");
 
     // get current view:
     uint8_t currentViewIndex = UI::VIEW_NAVIGATOR.getViewIndex();
     View* currentView = UI::VIEW_CONTROLLER.getView(currentViewIndex);
 
-    // run setup() of the current view if it's been switched recently:
+    // run set up current view if it's been switched recently:
     if (UI::VIEW_NAVIGATOR.getViewIndexChanged()) {
-        UI::VIEW_NAVIGATOR.resetViewIndexChanged();
-
         // check whether the current view exist (debugging):
         if (currentView == nullptr) {
             debug_print("err: loop: target view with index ");
@@ -77,9 +77,15 @@ void loop() {
             return;
         }
 
+        UI::VIEW_NAVIGATOR.resetViewIndexChanged();
         currentView->setup();
-        return;
+        UI::VIEW_RENDERER
+            .setRenderViewInstantly(); // the view will be rendered instantly instead of waiting for render interval
     }
+
+    // (re)render view (if needed):
+    uint32_t now = millis();
+    UI::VIEW_RENDERER.conditionallyRender(now, currentView);
 
     // run loop() for the current view:
     currentView->loop();
