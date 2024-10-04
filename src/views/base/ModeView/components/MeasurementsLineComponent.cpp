4d4036369c44a661d2fad63f023ed9bd59c64920
jieggii <jieggii@protonmail.com>
2024-10-04 18:51:09 +0300

    views/components: split MeasurementsLineComponent definition and implementation

---
+++ src/views/base/ModeView/components/MeasurementsLineComponent.cpp
@@ -0,0 +1,119 @@
+//
+// Created by jieggii on 10/4/24.
+//
+
+#include "MeasurementsLineComponent.h"
+
+/**
+ * Buffer for rendering measurements line on the display.
+ */
+class RenderBuffer {
+   public:
+    RenderBuffer() = default;
+
+    /**
+     * Format the buffer with the given measurements.
+     * If no measurements are provided, the buffer will be formatted as a template with no measurements displayed.
+     * TODO: rename this method to something more descriptive.
+     */
+    void format(const EnvSensorMeasurements* measurements = nullptr) {
+        // display temperature if measurements provided:
+        if (measurements) {
+            buffer[0] = static_cast<char>('0' + measurements->temperature / 10);
+            buffer[1] = static_cast<char>('0' + measurements->temperature % 10);
+        } else {
+            buffer[0] = ' ';
+            buffer[1] = ' ';
+        }
+        buffer[2] = DEGREE_SYMBOL;
+        buffer[3] = 'C';
+
+        buffer[4] = ' ';
+
+        // display humidity if measurements provided:
+        if (measurements) {
+            buffer[5] = static_cast<char>('0' + measurements->humidity / 10);
+            buffer[6] = static_cast<char>('0' + measurements->humidity % 10);
+        } else {
+            buffer[5] = ' ';
+            buffer[6] = ' ';
+        }
+        buffer[7] = '%';
+
+        buffer[8] = ' ';
+
+        // display CO2 if measurements provided:
+        if (measurements) {
+            buffer[9] = static_cast<char>('0' + measurements->co2 / 1000);
+            buffer[10] = static_cast<char>('0' + measurements->co2 % 1000 / 100);
+            buffer[11] = static_cast<char>('0' + measurements->co2 % 100 / 10);
+            buffer[12] = static_cast<char>('0' + measurements->co2 % 10);
+        } else {
+            buffer[9] = ' ';
+            buffer[10] = ' ';
+            buffer[11] = ' ';
+            buffer[12] = ' ';
+        }
+
+        buffer[13] = 'p';
+        buffer[14] = 'p';
+        buffer[15] = 'm';
+        buffer[16] = '\0';  // Null-terminate the string
+    }
+
+    [[nodiscard]] const char* getBuffer() const { return this->buffer; }
+
+   private:
+    static constexpr char DEGREE_SYMBOL = static_cast<char>(223);  // degree symbol ('°')
+
+    char buffer[DISPLAY_COLS + 1] = {};  // the actual buffer for rendering
+};
+
+void MeasurementsLineComponent::setMeasurements(const EnvSensorMeasurements measurements) {
+    this->measurements = measurements;
+}
+
+void MeasurementsLineComponent::setMeasurementStatusIconIDs(const MeasurementStatusIconIDs iconIDs) {
+    this->measurementStatusIconIDs = iconIDs;
+}
+
+void MeasurementsLineComponent::render(LCD1602* display) {
+    const uint32_t now = millis();
+
+    switch (this->state) {
+        case State::DISPLAY_MEASUREMENTS:
+            this->renderMeasurements(display);
+            if (this->displayMeasurementsTimer.isExpired(now)) {
+                this->state = State::DISPLAY_STATUS_ICONS;
+                this->displayStatusesTimer.set(now);
+            }
+            break;
+
+        case State::DISPLAY_STATUS_ICONS:
+            this->renderStatusIcons(display);
+
+            if (this->displayStatusesTimer.isExpired(now)) {
+                this->state = State::DISPLAY_MEASUREMENTS;
+                this->displayMeasurementsTimer.set(now);
+            }
+            break;
+    }
+}
+
+void MeasurementsLineComponent::renderMeasurements(LCD1602* display) const {
+    RenderBuffer renderBuffer;
+    renderBuffer.format(&this->measurements);
+
+    display->displayText(renderBuffer.getBuffer(), this->coordinates);
+}
+
+void MeasurementsLineComponent::renderStatusIcons(LCD1602* display) const {
+    RenderBuffer renderBuffer;
+    renderBuffer.format();
+
+    display->displayText(renderBuffer.getBuffer(), {0, 1});
+
+    display->displayIcon(this->measurementStatusIconIDs.temperature, {1, 1});
+    display->displayIcon(this->measurementStatusIconIDs.humidity, {6, 1});
+    display->displayIcon(this->measurementStatusIconIDs.co2, {12, 1});
+}
--- src/views/base/ModeView/components/MeasurementsLineComponent.h
+++ src/views/base/ModeView/components/MeasurementsLineComponent.h
@@ -7,78 +7,21 @@
 #include "Timer.h"
 #include "EnvSensor.h"
 
-class RenderBuffer {
-   public:
-    RenderBuffer() = default;
-
-    /**
-     * Format the buffer with the given measurements.
-     * If no measurements are provided, the buffer will be formatted as a template with no measurements displayed.
-     */
-    void format(const EnvSensorMeasurements* measurements = nullptr) {
-        // display temperature if available:
-        if (measurements) {
-            buffer[0] = static_cast<char>('0' + measurements->temperature / 10);
-            buffer[1] = static_cast<char>('0' + measurements->temperature % 10);
-        } else {
-            buffer[0] = ' ';
-            buffer[1] = ' ';
-        }
-        buffer[2] = DEGREE_SYMBOL;
-        buffer[3] = 'C';
-
-        buffer[4] = ' ';
-
-        // display humidity if available:
-        if (measurements) {
-            buffer[5] = static_cast<char>('0' + measurements->humidity / 10);
-            buffer[6] = static_cast<char>('0' + measurements->humidity % 10);
-        } else {
-            buffer[5] = ' ';
-            buffer[6] = ' ';
-        }
-        buffer[7] = '%';
-
-        buffer[8] = ' ';
-
-        // display CO2 if available:
-        if (measurements) {
-            buffer[9] = static_cast<char>('0' + measurements->co2 / 1000);
-            buffer[10] = static_cast<char>('0' + measurements->co2 % 1000 / 100);
-            buffer[11] = static_cast<char>('0' + measurements->co2 % 100 / 10);
-            buffer[12] = static_cast<char>('0' + measurements->co2 % 10);
-        } else {
-            buffer[9] = ' ';
-            buffer[10] = ' ';
-            buffer[11] = ' ';
-            buffer[12] = ' ';
-        }
-
-        buffer[13] = 'p';
-        buffer[14] = 'p';
-        buffer[15] = 'm';
-        buffer[16] = '\0';  // Null-terminate the string
-    }
-
-    [[nodiscard]] const char* getBuffer() const { return this->buffer; }
-
-   private:
-    static constexpr char DEGREE_SYMBOL = static_cast<char>(223);
-
-    char buffer[DISPLAY_COLS + 1] = {};
-};
-
 /**
  * MeasurementsLineComponent line displaying environmental measurements and their statuses.
  */
 class MeasurementsLineComponent final : public ViewComponent {
    public:
+    /**
+     * Icon IDs representing statuses of each measurement.
+     */
     struct MeasurementStatusIconIDs {
         uint8_t temperature;
         uint8_t humidity;
         uint8_t co2;
     };
 
+    // TODO: move constructor to the cpp file.
     MeasurementsLineComponent(const DisplayCoordinates coordinates, const uint16_t displayMeasurementsDuration,
                               const uint16_t displayStatusesDuration, const uint8_t defaultMeasurementStatusIconID)
         : ViewComponent(coordinates),
@@ -91,59 +34,21 @@ class MeasurementsLineComponent final : public ViewComponent {
     /**
      * Set measurements to be displayed.
      */
-    void setMeasurements(const EnvSensorMeasurements measurements) { this->measurements = measurements; }
+    void setMeasurements(EnvSensorMeasurements measurements);
 
     /**
      * Set icon IDs representing statuses of each measurement.
      */
-    void setMeasurementStatusIconIDs(const MeasurementStatusIconIDs iconIDs) {
-        this->measurementStatusIconIDs = iconIDs;
-    }
+    void setMeasurementStatusIconIDs(MeasurementStatusIconIDs iconIDs);
 
-    void renderMeasurements(LCD1602* display) const {
-        RenderBuffer renderBuffer;
-        renderBuffer.format(&this->measurements);
-
-        display->displayText(renderBuffer.getBuffer(), this->coordinates);
-    }
-
-    void renderStatusIcons(LCD1602* display) const {
-        RenderBuffer renderBuffer;
-        renderBuffer.format();
-
-        display->displayText(renderBuffer.getBuffer(), {0, 1});
-
-        display->displayIcon(this->measurementStatusIconIDs.temperature, {1, 1});
-        display->displayIcon(this->measurementStatusIconIDs.humidity, {6, 1});
-        display->displayIcon(this->measurementStatusIconIDs.co2, {12, 1});
-    }
-
-    void render(LCD1602* display) override {
-        const uint32_t now = millis();
-
-        switch (this->state) {
-            case State::DISPLAY_MEASUREMENTS:
-                this->renderMeasurements(display);
-                if (this->displayMeasurementsTimer.isExpired(now)) {
-                    this->state = State::DISPLAY_STATUS_ICONS;
-                    this->displayStatusesTimer.set(now);
-                }
-                break;
-
-            case State::DISPLAY_STATUS_ICONS:
-                this->renderStatusIcons(display);
-
-                if (this->displayStatusesTimer.isExpired(now)) {
-                    this->state = State::DISPLAY_MEASUREMENTS;
-                    this->displayMeasurementsTimer.set(now);
-                }
-                break;
-        }
-    }
+    /**
+     * Render the component.
+     */
+    void render(LCD1602* display) override;
 
    private:
-    Timer displayMeasurementsTimer;
-    Timer displayStatusesTimer;
+    Timer displayMeasurementsTimer;  // timer controlling how long measurements are displayed
+    Timer displayStatusesTimer;      // timer controlling how long measurement statuses are displayed
 
     MeasurementStatusIconIDs measurementStatusIconIDs;  // icon IDs representing statuses of each measurement
 
@@ -153,6 +58,16 @@ class MeasurementsLineComponent final : public ViewComponent {
     } state = State::DISPLAY_MEASUREMENTS;  // current state of the component
 
     EnvSensorMeasurements measurements = {.temperature = 0, .humidity = 0, .co2 = 0};  // current measurements
+
+    /**
+     * Render measurements.
+     */
+    void renderMeasurements(LCD1602* display) const;
+
+    /**
+     * Render measurement status icons.
+     */
+    void renderStatusIcons(LCD1602* display) const;
 };
 
 #endif  // MEASUREMENTSLINECOMPONENT_H
