710a606501eae937e68d0ecbfe3351b2c7f4ae9c
jieggii <jieggii@protonmail.com>
2024-10-05 14:22:01 +0300

    lib/rtc: create stub RTC lib

---
+++ lib/rtc/src/RTC.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jieggii on 10/5/24.
+//
+
+#include "RTC.h"
---
+++ lib/rtc/src/RTC.h
@@ -0,0 +1,15 @@
+//
+// Created by jieggii on 10/5/24.
+//
+
+#ifndef RTC_H
+#define RTC_H
+
+class RTC {
+   public:
+    RTC() = default;
+
+    void init() {}
+};
+
+#endif  // RTC_H
