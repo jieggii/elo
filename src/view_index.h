b50fa740708c545c9a76fc44f97384ae520b1896
jieggii <jieggii@protonmail.com>
2024-10-07 00:14:25 +0300

    move ViewIndex.h

---
+++ src/view_index.h
@@ -0,0 +1,15 @@
+//
+// Created by jieggii on 9/4/24.
+//
+
+#ifndef ELO_VIEWINDEX_H
+#define ELO_VIEWINDEX_H
+
+#include <cstdint>
+
+constexpr uint8_t IDLE_VIEW_INDEX = 0;
+constexpr uint8_t STAND_VIEW_INDEX = 1;
+constexpr uint8_t SIT_VIEW_INDEX = 2;
+constexpr uint8_t EXERCISE_VIEW_INDEX = 3;
+
+#endif  // ELO_VIEWINDEX_H
