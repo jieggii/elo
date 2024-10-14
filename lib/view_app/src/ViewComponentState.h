746979766b3deb30a9564e8dc2981477ed611bfb
jieggii <jieggii@protonmail.com>
2024-10-14 20:55:51 +0300

    lib/view_app: create basic ViewComponentState

---
+++ lib/view_app/src/ViewComponentState.h
@@ -0,0 +1,35 @@
+#ifndef ELO_VIEWCOMPONENTSTATE_H
+#define ELO_VIEWCOMPONENTSTATE_H
+
+/**
+ * Base class for view component states.
+ */
+class ViewComponentState {
+   public:
+    ViewComponentState() = default;
+
+    /**
+     * Hides the component.
+     */
+    virtual void hide() { this->hidden = true; };
+
+    /**
+     * Shows the component.
+     */
+    virtual void show() { this->hidden = false; };
+
+    /**
+     * Returns true if the component is hidden.
+     */
+    virtual bool isHidden() { return this->hidden; };
+
+    virtual ~ViewComponentState() = default;
+
+   private:
+    /**
+     * True if the component is hidden.
+     */
+    bool hidden = false;
+};
+
+#endif
