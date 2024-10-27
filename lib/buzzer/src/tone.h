8030617a5a830e0fa2341284c8c38201aea9d0b1
jieggii <jieggii@protonmail.com>
2024-10-27 17:39:10 +0200

    lib/buzzer: create `tone.h`

---
+++ lib/buzzer/src/tone.h
@@ -0,0 +1,50 @@
+//
+// Created by jieggii on 10/27/24.
+// This file contains different notes that can be played by the buzzer.
+//
+
+#ifndef TONES_H
+#define TONES_H
+
+#include <cstdint>
+
+namespace Tone {
+    constexpr uint16_t C3 = 130;
+    constexpr uint16_t C3_SHARP = 138;
+    constexpr uint16_t D3 = 146;
+    constexpr uint16_t D3_SHARP = 155;
+    constexpr uint16_t E3 = 164;
+    constexpr uint16_t F3 = 174;
+    constexpr uint16_t F3_SHARP = 185;
+    constexpr uint16_t G3 = 196;
+    constexpr uint16_t G3_SHARP = 207;
+    constexpr uint16_t A3 = 220;
+    constexpr uint16_t A3_SHARP = 233;
+    constexpr uint16_t B3 = 246;
+    constexpr uint16_t C4 = 262;
+    constexpr uint16_t C4_SHARP = 277;
+    constexpr uint16_t D4 = 293;
+    constexpr uint16_t D4_SHARP = 311;
+    constexpr uint16_t E4 = 330;
+    constexpr uint16_t F4 = 349;
+    constexpr uint16_t F4_SHARP = 369;
+    constexpr uint16_t G4 = 392;
+    constexpr uint16_t G4_SHARP = 415;
+    constexpr uint16_t A4 = 440;
+    constexpr uint16_t A4_SHARP = 466;
+    constexpr uint16_t B4 = 493;
+    constexpr uint16_t C5 = 523;
+    constexpr uint16_t C5_SHARP = 554;
+    constexpr uint16_t D5 = 587;
+    constexpr uint16_t D5_SHARP = 622;
+    constexpr uint16_t E5 = 659;
+    constexpr uint16_t F5 = 698;
+    constexpr uint16_t F5_SHARP = 739;
+    constexpr uint16_t G5 = 783;
+    constexpr uint16_t G5_SHARP = 830;
+    constexpr uint16_t A5 = 880;
+    constexpr uint16_t A5_SHARP = 932;
+    constexpr uint16_t B5 = 987;
+}  // namespace Tone
+
+#endif  // TONES_H
