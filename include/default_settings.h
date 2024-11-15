bf6c5da6b3d6234cde5926b5595d490081df7781
jieggii <jieggii@protonmail.com>
2024-11-15 10:28:55 +0200

    include/default_settings: set posture reminder interval to 30 sec

--- include/default_settings.h
+++ include/default_settings.h
@@ -18,8 +18,8 @@ namespace default_settings {
         .environment = {.temperature = {.optimal = {20.0f, 22.0f}, .acceptable = {19.0f, 23.5f}},
                         .humidity = {.optimal = {40, 60}, .acceptable = {30, 70}},
                         .co2 = {.max_optimal = 800, .max_acceptable = 900}},
-        .standWorkMode = {.postureReminder = {.enabled = true, .interval = 10UL * 1000}, .duration = 45UL * 60 * 1000},
-        .sitWorkMode = {.postureReminder = {.enabled = true, .interval = 3UL * 60 * 1000},
+        .standWorkMode = {.postureReminder = {.enabled = true, .interval = 30UL * 1000}, .duration = 45UL * 60 * 1000},
+        .sitWorkMode = {.postureReminder = {.enabled = true, .interval = 30UL * 1000},
                         .duration = 45UL * 60 * 1000},
     };
 }  // namespace default_settings
